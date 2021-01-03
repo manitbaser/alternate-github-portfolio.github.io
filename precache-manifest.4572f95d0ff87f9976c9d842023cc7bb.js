6ae7772510088da55279733697f85ac3802745f7
manitbaser <manitb06@gmail.com>
2021-01-03 14:08:40 +0530

    Updates

---
+++ precache-manifest.4572f95d0ff87f9976c9d842023cc7bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e661f621ea92ab43e42aa46546ebc922",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "88237ade162cc6a17e1d",
+    "url": "/home/static/css/main.bfc21801.chunk.css"
+  },
+  {
+    "revision": "8cb153b1973aa6006cba",
+    "url": "/home/static/js/2.00f28cd4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.00f28cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88237ade162cc6a17e1d",
+    "url": "/home/static/js/main.4ce96af8.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "3a6b3fe3ee4161d958e6b81aaae432ec",
+    "url": "/home/static/media/manitbaser.3a6b3fe3.png"
+  },
+  {
+    "revision": "553abd6a90b49f72dcb1222b99ad33d5",
+    "url": "/home/static/media/resume.553abd6a.pdf"
+  }
+]);
